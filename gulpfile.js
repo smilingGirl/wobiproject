a0b0f94c922f8c777dedbd2064ccd2118e5c6833
unknown <al97sch@yahoo.de>
2018-04-10 22:22:35 +0200

    install task added

--- gulpfile.js
+++ gulpfile.js
@@ -17,10 +17,26 @@ gulp.task('nodemon', function() {
 gulp.task('eslint', function() {
   return gulp.src('src/**/*.js')
     .pipe(eslint())
-    .pipe(eslint.reporter('eslint-stylish'));
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
   gulp.watch('src/**/*.js', ['eslint']);
 });
+
+// Install production dependencies
+function installNpmDependencies(done) {
+  return gulp.src('package.json')
+    .pipe(shell('npm install --production', {
+      cwd: targetDirectory,
+    }));
+}
+
+var installDependencies = gulp.series(installNpmDependencies);
+
+// Task definition
+gulp.task('build', gulp.series(/*clean, codeValidation, gulp.parallel(codeCompilation, codeDocumentation)*/));
+gulp.task('install', gulp.series('build', installDependencies));
+gulp.task('default', gulp.series('build'));
