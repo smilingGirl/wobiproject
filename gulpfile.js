4d65de36827f3450caeaaf7c1cc861a0f29f507b
unknown <al97sch@yahoo.de>
2018-04-10 22:06:59 +0200

    structure change

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var eslint = require('gulp-eslint');
 
 gulp.task('nodemon', function() {
   nodemon({
@@ -10,4 +11,16 @@ gulp.task('nodemon', function() {
   .on('restart', function() {
     console.log('>> node restart');
   })
+});
+
+// configure a eslint task
+gulp.task('eslint', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.reporter('eslint-stylish'));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.js', ['eslint']);
 });
