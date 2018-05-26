1e9025cd7ff777e194ef350711a6a6fd6b29bf97
unknown <al97sch@yahoo.de>
2018-05-26 15:48:37 +0200

    New Character, Country, Culture. Delete World.

--- README.md
+++ README.md
@@ -7,6 +7,43 @@ Have you ever struggled creating and planning a world for your creative writing?
 
 Available world components (so far):
 - World
+    - name
+    - status (work in progress Y/N)
+    - history (backend support only so far)
 - Characters
+    - first name
+    - last name
+    - age
+    - status (alive/dead)
+    - culture (backend support only so far)
+    - country (backend support only so far)
 - Countries/Lands
+    - name
+    - system (political)
+    - neighbours (backend support only so far)
+    - values (backend support only so far)
 - Cultures
+    - name
+    - believes (backend support only so far)
+    - holidays (backend support only so far)
+    - values (backend support only so far)
+
+API routes for version 1.0.0. :
+*** API [GET] /worlds registered
+*** API [POST] /worlds registered
+*** API [GET] /worlds/:worldID registered
+*** API [PUT] /worlds/:worldID registered
+*** API [DELETE] /worlds/:worldID registered
+*** API [GET] /:world/characters registered
+*** API [POST] /:world/characters registered
+*** API [PUT] /:world/characters/:characterID registered
+*** API [DELETE] /:world/characters/:characterID registered
+*** API [GET] /:world/characters/:characterID registered
+*** API [GET] /:world/countries registered
+*** API [POST] /:world/countries registered
+*** API [GET] /:world/countries/:countryID registered
+*** API [GET] /:world/cultures registered
+*** API [POST] /:world/cultures registered
+*** API [GET] /:world/cultures/:cultureID registered
+
+- not included so far: PUT and DELETE support for countries and cultures
--- src/app/nedit-form/wobi-form.component.ts
+++ src/app/nedit-form/wobi-form.component.ts
@@ -6,12 +6,15 @@ import { World, Country, Character, Culture } from '../model/schema';
 
 import { NgxSmartModalService } from 'ngx-smart-modal';
 import { WorldService } from '../services/world.service';
+import { CharacterService } from '../services/character.service';
+import { CultureService } from '../services/culture.service';
+import { CountryService } from '../services/country.service';
 
 @Component({
   selector: 'wobi-form',
   templateUrl: './wobi-form.component.html',
   styleUrls: ['./wobi-form.component.scss'],
-  providers: [WorldService]
+  providers: [WorldService, CharacterService, CultureService, CountryService]
 })
 
 //@Modal()
@@ -24,9 +27,18 @@ export class wobiFormComponent implements OnInit{
   country:string = '';
   culture:string = '';
   status:string = '';
+  system:string = '';
   wip:boolean = false;
+  private _selectedWorldId: number;
  
-  constructor(private fb: FormBuilder, private _dataService: WorldService, public ngxSmartModalService: NgxSmartModalService) {
+  constructor(
+    private fb: FormBuilder, 
+    private _dataService: WorldService, 
+    public ngxSmartModalService: NgxSmartModalService, 
+    private _charaService: CharacterService,
+    private _cultService: CultureService,
+    private _countSevice: CountryService 
+  ){
     this.wobiForm = new FormGroup({
       'name': this.name,
       country: new FormControl(),
@@ -34,11 +46,16 @@ export class wobiFormComponent implements OnInit{
       fname: new FormControl(),
       age: new FormControl(),
       status: new FormControl(),
+      system: new FormControl(),
       wip: new FormControl()
     }) 
   }
 
   @Input() selectedInputCharacteristic: string;
+  @Input() set selectedWorldId(value: number) {
+    this._selectedWorldId = value;
+    console.log(this._selectedWorldId);
+  }
 
   
   ngOnInit() {
@@ -46,10 +63,14 @@ export class wobiFormComponent implements OnInit{
 
   onSubmitModelBased() {
     console.log("model-based form submitted");
-    console.log(this.wobiForm);
     if (this.selectedInputCharacteristic  == "world") {
-      console.log(this.wobiForm.value);
       this.newWorld(this.wobiForm.value.name, this.wobiForm.value.wip);
+    } else if (this.selectedInputCharacteristic  == "character") {
+      this.newCharacter(this.wobiForm.value.fname, this.wobiForm.value.name, this.wobiForm.value.age, this.wobiForm.value.status, this._selectedWorldId);
+    } else if (this.selectedInputCharacteristic == "culture") {
+      this.newCulture(this.wobiForm.value.name, this._selectedWorldId);
+    } else if (this.selectedInputCharacteristic == "country") {
+      this.newCountry(this.wobiForm.value.name, this.wobiForm.value.system, this._selectedWorldId);
     }
     this.ngxSmartModalService.getModal('neditFormModal').close();
   }
@@ -64,7 +85,47 @@ export class wobiFormComponent implements OnInit{
     this._dataService.createWorld(newW).subscribe(data => {
       console.log(data);
     }, error => {
-      alert('Failed fetching worlds');
+      alert('Failed creating a new world');
+    });
+  }
+
+  private newCharacter(firstname, lastname, age, status, worldID) {
+    var newC = <Character>{};
+    newC.lastName = lastname;
+    newC.firstName = firstname;
+    newC.age = age;
+    newC.status = status;
+    newC.worldID = worldID;
+
+    this._charaService.createCharacter(newC).subscribe(data => {
+      console.log(data);
+    }, error => {
+      alert('Failed creating a new character');
+    });
+  }
+
+  private newCountry(name,system, worldID ) {
+    var newC = <Country>{};
+    newC.name = name;
+    newC.system = system;
+    newC.worldID = worldID;
+
+    this._countSevice.createCountry(newC).subscribe(data => {
+      console.log(data);
+    }, error => {
+      alert('Failed creating a new country');
+    });
+  }
+
+  private newCulture(name, worldID ) {
+    var newC = <Culture>{};
+    newC.name = name;
+    newC.worldID = worldID;
+
+    this._cultService.createCulture(newC).subscribe(data => {
+      console.log(data);
+    }, error => {
+      alert('Failed creating a new culture');
     });
   }
 
